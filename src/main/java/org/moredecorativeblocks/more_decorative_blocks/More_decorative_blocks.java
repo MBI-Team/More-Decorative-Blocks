c0c497560c2395c23f42ae75ba1030f129fe74f8
蛋黄酥danhuangsu <3677661491@qq.com>
2025-04-26 22:10:35 +0800

    Update mbd_block's texture

--- src/main/java/org/moredecorativeblocks/more_decorative_blocks/More_decorative_blocks.java
+++ src/main/java/org/moredecorativeblocks/more_decorative_blocks/More_decorative_blocks.java
@@ -49,9 +49,14 @@ public class More_decorative_blocks {
     // Creates a new BlockItem with the id "more_decorative_blocks:MDB_block", combining the namespace and path
     public static final RegistryObject<Item> MDB_BLOCK_ITEM = ITEMS.register("mdb_block", () -> new BlockItem(MDB_BLOCK.get(), new Item.Properties()));
 
-    // Creates a creative tab with the id "more_decorative_blocks:example_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> MDB_BLOCK_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(MDB_BLOCK_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+    public static final RegistryObject<Block> FIRE_BOOK = BLOCKS.register("fire_book", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+
+    public static final RegistryObject<Item> FIRE_BOOK_ITEM = ITEMS.register("fire_book", () -> new BlockItem(FIRE_BOOK.get(), new Item.Properties()));
+
+    // Creates a creative tab with the id "more_decorative_blocks:mdb_tab" for the example item, that is placed after the combat tab
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("mdb_tab", () -> CreativeModeTab.builder().withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> MDB_BLOCK_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
+        output.accept(MDB_BLOCK_ITEM.get());
+        output.accept(FIRE_BOOK_ITEM.get());// Add the example item to the tab. For your own tabs, this method is preferred over the event
     }).build());
 
     public More_decorative_blocks() {
