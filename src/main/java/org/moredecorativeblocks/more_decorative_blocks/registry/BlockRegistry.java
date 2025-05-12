28a8b7997985cde195dd43d2bae90a9aa5a9342b
蛋黄酥danhuangsu <3897081534@qq.com>
2025-05-13 00:53:49 +0800

    recipes done
    Signed-off-by: 蛋黄酥danhuangsu <3897081534@qq.com>

--- src/main/java/org/moredecorativeblocks/more_decorative_blocks/registry/BlockRegistry.java
+++ src/main/java/org/moredecorativeblocks/more_decorative_blocks/registry/BlockRegistry.java
@@ -16,16 +16,16 @@ public class BlockRegistry {
 
     public static final DeferredBlock<Block> MDB_BLOCK = BLOCKS.register("mdb_block", () -> new BlockFacing(BlockBehaviour.Properties.of()
             .mapColor(MapColor.STONE)
-            .sound(SoundType.NETHERITE_BLOCK) // 新型金属声效
+            .sound(SoundType.NETHERITE_BLOCK)
             .lightLevel(state -> 8)
-            .strength(2.5f,10.0f)  // 硬度参数（可选）
-            .requiresCorrectToolForDrops()  // 需要正确工具采集（可选）
+            .strength(1.5f, 3.0f)  // 硬度参数（可选）
             .noOcclusion()  // 关闭面剔除（谨慎使用，可能导致透视问题）
             .isRedstoneConductor((state, level, pos) -> true)
     ));
 
     public static final DeferredBlock<Block> WATER_BOOK = BLOCKS.register("water_book", () -> new BlockFacing(BlockBehaviour.Properties.of()
             .mapColor(MapColor.STONE)
+            .sound(SoundType.COPPER)
             .lightLevel(state -> 8)
             .strength(1.0f,0.5f)  // 硬度参数（可选）
             .noOcclusion()  // 关闭面剔除（谨慎使用，可能导致透视问题）
@@ -33,6 +33,7 @@ public class BlockRegistry {
 
     public static final DeferredBlock<Block> FIRE_BOOK = BLOCKS.register("fire_book", () -> new BlockFacing(BlockBehaviour.Properties.of()
             .mapColor(MapColor.STONE)
+            .sound(SoundType.COPPER)
             .lightLevel(state -> 8)
             .strength(1.0f,0.5f)  // 硬度参数（可选）
             .noOcclusion()  // 关闭面剔除（谨慎使用，可能导致透视问题）
@@ -40,8 +41,9 @@ public class BlockRegistry {
 
     public static final DeferredBlock<Block> SEAT_STONE = BLOCKS.register("seat_stone", () -> new BlockFacing(BlockBehaviour.Properties.of()
             .mapColor(MapColor.STONE)
+            .sound(SoundType.NETHERITE_BLOCK)
             .lightLevel(state -> 1)
-            .strength(2.0f, 8f)  // 硬度参数（可选）
+            .strength(1.5f, 3f)  // 硬度参数（可选）
             .noOcclusion()  // 关闭面剔除（谨慎使用，可能导致透视问题）
     ));
 
