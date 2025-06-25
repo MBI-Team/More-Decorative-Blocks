277e73188885120dad611abee737d4559e302bd6
danhuangsu3677661491 <3677661491@qq.com>
2025-06-25 20:34:53 +0800

    optimize texture,recipes,loot tables.
    Signed-off-by: danhuangsu3677661491 <3677661491@qq.com>

--- push.cmd
+++ push.cmd
@@ -1,2 +1,3 @@
 git push --progress --porcelain origin refs/heads/NeoForge1.21:NeoForge1.21
+git push --progress --porcelain origin refs/heads/NeoForge1.21-stable:NeoForge1.21-stable
 git push --progress --porcelain origin refs/heads/Forge1.21:Forge1.21
