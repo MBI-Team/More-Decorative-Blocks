4b7bec383bbd3fb69ec6121be34c2772b1d20c91
danhuangsu3677661491 <3677661491@qq.com>
2025-09-21 13:48:22 +0800

    a
    Signed-off-by: danhuangsu3677661491 <3677661491@qq.com>

--- src/main/java/org/more_blocks_and_items_team/more_decorative_blocks/objects/fluid/MudFluidType.java
+++ src/main/java/org/more_blocks_and_items_team/more_decorative_blocks/objects/fluid/MudFluidType.java
@@ -1,9 +1,9 @@
 package org.more_blocks_and_items_team.more_decorative_blocks.objects.fluid;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.pathfinder.PathType;
 import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.neoforged.neoforge.fluids.FluidType;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
@@ -21,30 +21,14 @@ public class MudFluidType extends FluidType {
     public void initializeClient(Consumer<IClientFluidTypeExtensions> consumer) {
         consumer.accept(new IClientFluidTypeExtensions() {
             @Override
-            public ResourceLocation getStillTexture() {
+            public @NotNull ResourceLocation getStillTexture() {
                 return stillTexture;
             }
 
             @Override
-            public ResourceLocation getFlowingTexture() {
+            public @NotNull ResourceLocation getFlowingTexture() {
                 return flowingTexture;
             }
         });
-    }
-
-    public boolean canSwim() {
-        return false;
-    }
-
-    public boolean canDrown() {
-        return true;
-    }
-
-    public PathType getPathType() {
-        return PathType.WATER;
-    }
-
-    public PathType getAdjacentPathType() {
-        return PathType.WATER;
     }
 }
